d9ae2dad70993070d010f95e6b5cb7a453ca7bb4
Marithza Erica Pierre <mpierre1@uespoir.com>
2026-03-27 11:14:30 -0700

    Add main logic with EmployeeForm and EmployeeList

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
+// import { useState } from "react"  
+// import EmployeeForm from "./components/EmployeeForm"
+// import EmployeeList from "./components/EmployeeList"
 
-import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to My React App!</h1>
-      <p>This is a simple React application.</p>
-    </div>
-  )
-}
-
-export default App
+// function App() {}
